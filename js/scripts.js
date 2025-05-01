3f9f681bd8a4d590f054d43886b01a5833dee13b
Dylan-B-05 <20112375@mail.wit.ie>
2025-05-01 14:09:28 +0000

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
 console.log("Page loaded");
 
 function addToLikes() {
-  alert('Added to Likes');
-}
-
-function addToCart() {
-  alert('Item added to cart');
-}
-
-function removeFromCart() {
-  alert('Removed from cart');
-}
-
-function submitReview() {
-  alert('Thank you for your review!');
-}
-
-function submitPayment() {
-  window.location.href = 'review.html';
-}
+    alert("Added to Likes");
+  }
+  
+  function addToCart() {
+    alert("Item added to cart");
+  }
+  
+  function removeFromCart() {
+    alert("Removed from cart");
+  }
+  
+  function submitReview() {
+    alert("Thank you for your review!");
+  }
+  
+  function submitPayment() {
+    window.location.href = "review.html";
+  }
+  
